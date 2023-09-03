575f169d00a44ae70800dfc5bd38bfd38d6abeee
DESKTOP-F8S35N2\bryce <brycepc@hotmail.com>
2023-09-03 19:58:31 +1200

    Refactor for nodejs18

--- src/application/lambdas/get-orders/container.ts
+++ src/application/lambdas/get-orders/container.ts
@@ -10,12 +10,15 @@ import {IOrderCommandHandler} from '../../handlers/order-command.handler.interfa
 import {OrderCommandHandler} from '../../handlers/order-command.handler';
 import {IEventBridgeClient} from '../../../infrastructure/interfaces/eventbridge-client.interface';
 import {EventBridgeClient} from '../../../infrastructure/adapters/eventbridge-client';
+import {IRestApiClient} from '../../../infrastructure/interfaces/restapi-client.interface';
+import {RestApiClient} from '../../../infrastructure/adapters/restapi-client';
 
 const container = new Container();
 
 container.bind<IOrderRepository>(TYPES.IOrderRepository).to(OrderRepository).inSingletonScope();
 container.bind<IAppOrderService>(TYPES.IAppOrderService).to(AppOrderService).inSingletonScope();
 container.bind<IOrderCommandHandler>(TYPES.IOrderCommandHandler).to(OrderCommandHandler).inSingletonScope();
+container.bind<IRestApiClient>(TYPES.IRestApiClient).to(RestApiClient).inSingletonScope();
 container.bind<IEventBridgeClient>(TYPES.IEventBridgeClient).to(EventBridgeClient).inSingletonScope();
 container.bind<IDynamoDBClient>(TYPES.IDynamoDBClient).to(DynamoDBClient).inSingletonScope();
 
